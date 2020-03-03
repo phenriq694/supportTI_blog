f6f35e9ce53cfd6662f846cfdd8dfa4b5ac6c580
Paulo Henrique <phenriq694@gmail.com>
2020-03-03 17:21:49 -0300

    BACKEND - Configuração do ESLint e Prettier.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express')
+const { Router } = require('express');
 
-const routes = new Router()
+const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
-})
+  return res.json({ message: 'Hello World' });
+});
 
-module.exports = routes
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-const app = require('./app')
+const app = require('./app');
 
-//Passando como parâmetro o número da porta onde o servidor vai ouvir requisições.
-app.listen(3333)
+// Passando como parâmetro o número da porta onde o servidor vai ouvir requisições.
+app.listen(3333);
